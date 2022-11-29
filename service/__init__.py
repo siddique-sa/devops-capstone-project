7085c1615553ef14233a2854b9c827ca44a0f11d
siddique-sa <siddiqueahmed.sa@gmail.com>
2022-11-29 02:23:42 -0500

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -8,10 +8,12 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+talisman = Talisman(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
@@ -35,3 +37,4 @@ except Exception as error:  # pylint: disable=broad-except
     sys.exit(4)
 
 app.logger.info("Service initialized!")
+
